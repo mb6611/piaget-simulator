6e24a5905a0eabe6053fde464b5a30dd78ae1c7a
Matthew Banaag <mb6611@princeton.edu>
2023-12-03 17:57:01 -0500

    messed with backgrounds

--- components/Chat/ChatMessage.tsx
+++ components/Chat/ChatMessage.tsx
@@ -128,13 +128,13 @@ export const ChatMessage: FC<Props> = memo(({ message, messageIndex, onEdit }) =
     <div
       className={`group md:px-4 ${
         message.role === 'assistant'
-          ? 'border-b border-black/10 bg-gray-50 text-gray-800 dark:border-gray-900/50 dark:bg-[#444654] dark:text-gray-100'
-          : 'border-b border-black/10 bg-white text-gray-800 dark:border-gray-900/50 dark:bg-[#343541] dark:text-gray-100'
+          ? 'border-b border-black/10 bg-gray-50 text-[#706233] dark:border-gray-900/50 dark:bg-[#B0926A] dark:text-[#706233]'
+          : 'border-b border-black/10 bg-white text-[#706233] dark:border-gray-900/50 dark:bg-[#E1C78F] dark:text-[#706233]'
       }`}
       style={{ overflowWrap: 'anywhere' }}
     >
       <div className="relative m-auto flex p-4 text-base md:max-w-2xl md:gap-6 md:py-6 lg:max-w-2xl lg:px-0 xl:max-w-3xl">
-        <div className="min-w-[40px] text-right font-bold">
+        <div className="min-w-[40px] text-right font-bold text-[#706233]">
           {message.role === 'assistant' ? (
             <IconRobot size={30} />
           ) : (
@@ -142,7 +142,7 @@ export const ChatMessage: FC<Props> = memo(({ message, messageIndex, onEdit }) =
           )}
         </div>
 
-        <div className="prose mt-[-2px] w-full dark:prose-invert">
+        <div className="mt-[-2px] w-full">
           {message.role === 'user' ? (
             <div className="flex w-full">
               {isEditing ? (
@@ -167,14 +167,14 @@ export const ChatMessage: FC<Props> = memo(({ message, messageIndex, onEdit }) =
 
                   <div className="mt-10 flex justify-center space-x-4">
                     <button
-                      className="h-[40px] rounded-md bg-blue-500 px-4 py-1 text-sm font-medium text-white enabled:hover:bg-blue-600 disabled:opacity-50"
+                      className="h-[40px] rounded-md bg-blue-500 px-4 py-1 text-sm font-medium text-[#706233] enabled:hover:bg-blue-600 disabled:opacity-50"
                       onClick={handleEditMessage}
                       disabled={messageContent.trim().length <= 0}
                     >
                       {t('Save & Submit')}
                     </button>
                     <button
-                      className="h-[40px] rounded-md border border-neutral-300 px-4 py-1 text-sm font-medium text-neutral-700 hover:bg-neutral-100 dark:border-neutral-700 dark:text-neutral-300 dark:hover:bg-neutral-800"
+                      className="h-[40px] rounded-md border border-neutral-300 px-4 py-1 text-sm font-medium text-[#706233] hover:bg-neutral-100 dark:border-neutral-700 dark:text-[#706233] dark:hover:bg-neutral-800"
                       onClick={() => {
                         setMessageContent(message.content);
                         setIsEditing(false);
@@ -193,13 +193,13 @@ export const ChatMessage: FC<Props> = memo(({ message, messageIndex, onEdit }) =
               {!isEditing && (
                 <div className="md:-mr-8 ml-1 md:ml-0 flex flex-col md:flex-row gap-4 md:gap-1 items-center md:items-start justify-end md:justify-start">
                   <button
-                    className="invisible group-hover:visible focus:visible text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-300"
+                    className="invisible group-hover:visible focus:visible text-[#706233] hover:text-[#706233] dark:text-[#706233] dark:hover:text-[#706233]"
                     onClick={toggleEditing}
                   >
                     <IconEdit size={20} />
                   </button>
                   <button
-                    className="invisible group-hover:visible focus:visible text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-300"
+                    className="invisible group-hover:visible focus:visible text-[#706233] hover:text-[#706233] dark:text-[#706233] dark:hover:text-[#706233]"
                     onClick={handleDeleteMessage}
                   >
                     <IconTrash size={20} />
@@ -247,14 +247,14 @@ export const ChatMessage: FC<Props> = memo(({ message, messageIndex, onEdit }) =
                   },
                   th({ children }) {
                     return (
-                      <th className="break-words border border-black bg-gray-500 px-3 py-1 text-white dark:border-white">
+                      <th className="break-words border border-black bg-gray-500 px-3 py-1 text-[#706233] dark:text-[#706233] dark:border-white">
                         {children}
                       </th>
                     );
                   },
                   td({ children }) {
                     return (
-                      <td className="break-words border border-black px-3 py-1 dark:border-white">
+                      <td className="break-words border border-black px-3 py-1 text-[#706233] dark:border-white">
                         {children}
                       </td>
                     );
@@ -274,7 +274,7 @@ export const ChatMessage: FC<Props> = memo(({ message, messageIndex, onEdit }) =
                   />
                 ) : (
                   <button
-                    className="invisible group-hover:visible focus:visible text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-300"
+                    className="invisible group-hover:visible focus:visible text-[#706233] hover:text-[#706233] dark:text-[#706233] dark:hover:text-[#706233]"
                     onClick={copyOnClick}
                   >
                     <IconCopy size={20} />
